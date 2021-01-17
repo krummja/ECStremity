a7c73274aa7300743be5c76ef5cd9e09cc549286
Jonathan Crum <crumja@uga.edu>
2021-01-16 21:03:04 -0500

    Cleaning up repo

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Building..."
-
-python3 setup.py bdist_wheel
-sudo pip3 install --upgrade ./dist/ecstremity-0.0.1-py3-none-any.whl
-
-echo "Done!"
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Starting tests..."
-
-python3 ./tests/unit.py
-
-echo "Done!"
